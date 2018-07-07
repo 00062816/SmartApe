95a6136322350de902e275ae37954d5802ce7c48
emartinezs <00116316@uca.edu.sv>
2018-07-07 17:42:43 -0600

    Se pueden agregar quizzes localmente

--- app/src/main/java/com/example/alexbig/smartape/activities/CreateQuizActivity.java
+++ app/src/main/java/com/example/alexbig/smartape/activities/CreateQuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexbig.smartape.activities;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.alexbig.smartape.R;
+import com.example.alexbig.smartape.database.viewmodels.QuizViewModel;
 import com.example.alexbig.smartape.models.Quiz;
 
 public class CreateQuizActivity extends AppCompatActivity{
@@ -17,17 +19,23 @@ public class CreateQuizActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_create_quiz);
 
-        EditText titleEditText = findViewById(R.id.editText_createQuiz_quizName);
-        EditText decriptionEditText = findViewById(R.id.editText_createQuiz_quizDescription);
-        Spinner categorySpinner = findViewById(R.id.spinner_createQuiz_quizCategory);
+        final EditText titleEditText = findViewById(R.id.editText_createQuiz_quizName);
+        final EditText descriptionEditText = findViewById(R.id.editText_createQuiz_quizDescription);
+        final Spinner categorySpinner = findViewById(R.id.spinner_createQuiz_quizCategory);
         Button createButton = findViewById(R.id.button_createQuiz);
 
-        Quiz quiz = new Quiz();
+        final Quiz quiz = new Quiz();
+        final QuizViewModel quizViewModel = ViewModelProviders.of(this).get(QuizViewModel.class);
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                quiz.setTitle(titleEditText.getText().toString());
+                quiz.setDescription(descriptionEditText.getText().toString());
+                quiz.setCategory(categorySpinner.getSelectedItem().toString());
+                quiz.setCreator("USER");
+                quizViewModel.insertQuiz(quiz);
+                finish();
             }
         });
     }
