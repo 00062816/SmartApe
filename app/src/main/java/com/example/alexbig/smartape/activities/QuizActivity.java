ef98964d1cf8a88c739c21b62b9a89ba7cbe2e46
emartinezs <00116316@uca.edu.sv>
2018-07-10 00:00:06 -0600

    Correccion de errores y funcionalidad para editar y quitar preguntas y respuestas

--- app/src/main/java/com/example/alexbig/smartape/activities/QuizActivity.java
+++ app/src/main/java/com/example/alexbig/smartape/activities/QuizActivity.java
@@ -69,7 +69,12 @@ public class QuizActivity extends AppCompatActivity{
         questionTextView = findViewById(R.id.textView_answerQuestion_question);
         RecyclerView recyclerView = findViewById(R.id.recyclerView_answerQuestion_answers);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        answerAdapter = new AnswerAdapter(this);
+        answerAdapter = new AnswerAdapter(this) {
+            @Override
+            public void onItemClick(View v, int position) {
+
+            }
+        };
         answerAdapter.setAnswerList(new ArrayList<>());
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(answerAdapter);
